75c759af5dd999a121544b86fb72134e1959311e
Azlan Ehtasham <azlanehtesham02@gmail.com>
2026-03-25 09:54:01 +0530

    Fix PrismaClient instantiation with datasourceUrl

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -2,6 +2,8 @@ import { env } from "@elector/env/server";
 
 import { PrismaClient } from "../prisma/generated/client";
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+  datasourceUrl: env.DATABASE_URL,
+});
 
 export default prisma;
