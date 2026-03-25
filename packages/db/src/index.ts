8bfa967ba1a0434b82df614c68772cfafbeaf44a
Azlan Ehtasham <azlanehtesham02@gmail.com>
2026-03-25 09:52:37 +0530

    Remove PrismaPg adapter for better Vercel serverless compatibility

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,9 +1,7 @@
 import { env } from "@elector/env/server";
-import { PrismaPg } from "@prisma/adapter-pg";
 
 import { PrismaClient } from "../prisma/generated/client";
 
-const adapter = new PrismaPg({ connectionString: env.DATABASE_URL });
-const prisma = new PrismaClient({ adapter });
+const prisma = new PrismaClient();
 
 export default prisma;
